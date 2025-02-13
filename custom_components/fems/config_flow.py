1e618b157d63bd4f6ce17ae4d8ec9430205b3f5d
alpenfun <maier.fm@gmail.com>
2025-02-13 20:07:44 +0100

    release

--- custom_components/fems/config_flow.py
+++ custom_components/fems/config_flow.py
@@ -1,9 +1,36 @@
-"""Config Flow für die Fenecon FEMS Integration."""
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.core import HomeAssistant
+from homeassistant.core import callback
 from homeassistant.helpers.selector import TextSelector, TextSelectorConfig, TextSelectorType
 from .const import DOMAIN
+
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Konfigurationsflow für Fenecon FEMS."""
+
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        """Einstiegspunkt für die UI-Konfiguration."""
+        errors = {}
+
+        if user_input is not None:
+            return self.async_create_entry(title="Fenecon FEMS", data=user_input)
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Required("modbus_host", default="192.168.11.104"): str,
+                vol.Required("modbus_port", default=502): int,
+                vol.Required("rest_url", default="http://192.168.11.104:8084"): str,
+            }),
+            errors=errors
+        )
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        """Ruft den OptionsFlow auf."""
+        return FeneconOptionsFlowHandler(config_entry)
 
 class FeneconOptionsFlowHandler(config_entries.OptionsFlow):
     """Options flow für Fenecon FEMS."""
@@ -14,7 +41,7 @@ class FeneconOptionsFlowHandler(config_entries.OptionsFlow):
     async def async_step_init(self, user_input=None):
         """Startet den Optionen-Dialog."""
         if user_input is not None:
-            return self.async_create_entry(title="", options=user_input)
+            return self.async_create_entry(title="", data=user_input)
 
         config_data = self.config_entry.options or {}
 
@@ -26,25 +53,3 @@ class FeneconOptionsFlowHandler(config_entries.OptionsFlow):
                 vol.Required("rest_url", default=config_data.get("rest_url", "http://192.168.11.104:8084")): str,
             })
         )
-
-class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Konfigurationsflow für Fenecon FEMS."""
-
-    async def async_step_user(self, user_input=None):
-        """Einstiegspunkt für die UI-Konfiguration."""
-        if user_input is not None:
-            return self.async_create_entry(title="Fenecon FEMS", data=user_input, options=user_input)
-
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema({
-                vol.Required("modbus_host", default="192.168.11.104"): str,
-                vol.Required("modbus_port", default=502): int,
-                vol.Required("rest_url", default="http://192.168.11.104:8084"): str,
-            })
-        )
-
-    @staticmethod
-    def async_get_options_flow(config_entry):
-        """Ruft den OptionsFlow auf."""
-        return FeneconOptionsFlowHandler(config_entry)
