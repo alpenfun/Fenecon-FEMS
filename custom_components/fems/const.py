ea5e520079e588fcd5d3a4732d3498409c7f4377
alpenfun <maier.fm@gmail.com>
2026-03-21 17:37:19 +0100

    Add MODBUS registers for energy monitoring

--- custom_components/fems/const.py
+++ custom_components/fems/const.py
@@ -46,9 +46,34 @@ REST_CHARGER1_CHANNELS = [
     "charger1/Current",
 ]
 
-MODBUS_REGISTERS = {
+MODBUS_UINT16_INPUT_REGISTERS = {
+    "ess_soc": 302,
+}
+
+MODBUS_FLOAT32_HOLDING_REGISTERS = {
     "ess_active_power": 303,
     "grid_active_power": 315,
     "production_dc_actual_power": 339,
     "consumption_active_power": 343,
+    "ess_active_power_l1": 391,
+    "ess_active_power_l2": 393,
+    "ess_active_power_l3": 395,
+    "grid_active_power_l1": 397,
+    "grid_active_power_l2": 399,
+    "grid_active_power_l3": 401,
+    "consumption_active_power_l1": 409,
+    "consumption_active_power_l2": 411,
+    "consumption_active_power_l3": 413,
+    "ess_discharge_power": 415,
+}
+
+MODBUS_FLOAT64_HOLDING_REGISTERS = {
+    "ess_active_charge_energy": 351,
+    "ess_active_discharge_energy": 355,
+    "grid_buy_active_energy": 359,
+    "grid_sell_active_energy": 363,
+    "production_active_energy": 367,
+    "consumption_active_energy": 379,
+    "ess_dc_charge_energy": 383,
+    "ess_dc_discharge_energy": 387,
 }
